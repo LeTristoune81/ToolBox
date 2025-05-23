a8d830484d56fde51ab44b68f8d07a6a8140616b
LeTristoune81 <pierre.pitou81@gmail.com>
2025-05-23 14:11:00 +0200

    ToolBox.user.js

--- dist/ToolBox.user.js
+++ dist/ToolBox.user.js
@@ -7,11 +7,14 @@
 // @match        *://*.fourmizzz.fr/*
 // @grant        GM_xmlhttpRequest
 // @grant        GM_info
+// @grant        GM_getResourceText
+// @grant        GM_addStyle
 // @connect      s1.fourmizzz.fr
 // @connect      s2.fourmizzz.fr
 // @connect      s3.fourmizzz.fr
 // @connect      discord.com
 // @run-at       document-idle
+// @resource     colonieCss https://raw.githubusercontent.com/LeTristoune81/ToolBox/main/css/colonie.css
 // @require      https://code.jquery.com/jquery-3.6.0.min.js
 // @require      https://cdnjs.cloudflare.com/ajax/libs/numeral.js/2.0.6/numeral.min.js
 // @require      https://raw.githubusercontent.com/LeTristoune81/ToolBox/main/src/utils.js
@@ -21,3 +24,6 @@
 // @require      https://raw.githubusercontent.com/LeTristoune81/ToolBox/main/src/ui.js
 // @require      https://raw.githubusercontent.com/LeTristoune81/ToolBox/main/src/main.js
 // ==/UserScript==
+
+// ─── Injecte uniquement le CSS des colonies depuis le fichier séparé ──────────
+GM_addStyle(GM_getResourceText('colonieCss'));
